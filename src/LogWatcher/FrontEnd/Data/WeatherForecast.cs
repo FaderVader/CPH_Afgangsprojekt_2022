08b04ed42c739015ee7e51d7c8ecca0d022358b3
FaderVader <jakobviggohansen@gmail.com>
2022-11-23 10:59:36 +0100

    swichted to core3.1 for LogWatcher

--- src/LogWatcher/FrontEnd/Data/WeatherForecast.cs
+++ src/LogWatcher/FrontEnd/Data/WeatherForecast.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace FrontEnd.Data
 {
     public class WeatherForecast
@@ -8,6 +10,6 @@ namespace FrontEnd.Data
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string Summary { get; set; }
     }
 }
